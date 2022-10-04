eed2bf4afcc1ac0efb89cc5ff1728fb415863b7a
darkbluea <blueavideo@gmail.com>
2022-10-04 16:43:00 +0200

    change and test base files for modding

--- src/main/java/net/fabricmc/modtest/ModTest.java
+++ src/main/java/net/fabricmc/modtest/ModTest.java
@@ -1,11 +1,10 @@
-/*
-package net.fabricmc.example;
+package net.fabricmc.modtest;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class ModTest implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -20,5 +19,3 @@ public class ExampleMod implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
 	}
 }
-
-*/
--- src/main/java/net/fabricmc/modtest/mixin/ModTestMixin.java
+++ src/main/java/net/fabricmc/modtest/mixin/ModTestMixin.java
@@ -1,7 +1,6 @@
-/*
-package net.fabricmc.example.mixin;
+package net.fabricmc.modtest.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.fabricmc.modtest.ModTest;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,10 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ModTestMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixisdazedsaqdsn!");
+		ModTest.LOGGER.info("This line is printed by an example mod mixisdazedsaqdsn!");
 	}
 }
-*/
